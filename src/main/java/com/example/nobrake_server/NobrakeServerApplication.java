63e59937a3abbeb38c56581b729b010d1c042431
strongcookdas <goodjav141592@gmail.com>
2026-03-08 20:50:48 +0900

    [server] feat : 프로젝트설정

---
+++ src/main/java/com/example/nobrake_server/NobrakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nobrake_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobrakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NobrakeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nobrake_server/NobrakeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nobrake_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobrakeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
